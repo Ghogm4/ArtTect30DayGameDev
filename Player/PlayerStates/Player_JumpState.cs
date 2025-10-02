9446a2aa14ce6da837e3fdadd9519dc7371aaedd
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2025-10-02 11:50:58 +0800

    Added rise and fall animation for player

--- Player/PlayerStates/Player_JumpState.cs
+++ Player/PlayerStates/Player_JumpState.cs
@@ -3,5 +3,32 @@ using System;
 
 public partial class Player_JumpState : State
 {
+    [Export] private Timer _riseToFallTimer = null;
+    private AnimatedSprite2D _sprite = null;
+    private Player _player = null;
 
+    protected override void ReadyBehavior()
+    {
+        _sprite = Storage.GetNode<AnimatedSprite2D>("AnimatedSprite");
+        _player = Storage.GetNode<Player>("Player");
+        _riseToFallTimer.Timeout += OnRiseToFallTimerTimeout;
+    }
+    protected override void Enter()
+    {
+        _sprite.Play("Rise");
+        _riseToFallTimer.Start();
+    }
+    protected override void FrameUpdate(double delta)
+    {
+        if (_player.IsOnFloor())
+            AskTransit("Idle");
+    }
+    private void OnRiseToFallTimerTimeout()
+    {
+        _sprite.Play("Fall");
+    }
+    protected override void Exit()
+    {
+        _riseToFallTimer.Stop();
+    }
 }
