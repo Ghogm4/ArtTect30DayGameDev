3e6050aec83311d005712f9afe3beac8a7a7e6ff
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2025-10-02 21:40:47 +0800

    Added coyote time and jump buffer to optimize the control of the player

--- Player/PlayerStates/Player_MoveControlState.cs
+++ Player/PlayerStates/Player_MoveControlState.cs
@@ -3,23 +3,45 @@ using System;
 
 public partial class Player_MoveControlState : State
 {
+	[Export] private Timer _jumpBufferTimer = null;
+	[Export] private Timer _coyoteTimer = null;
 	public const float Speed = 150.0f;
 	public const float JumpVelocity = 250.0f;
 	private Player _player = null;
+	private bool _isOnFloor = false;
+	private bool _isCoyoteTimerRunning = false;
+	private bool _canCoyoteTimerStart = true;
+	private bool _canJump = false;
+	private bool _willJump = false;
 	protected override void ReadyBehavior()
 	{
 		_player = Storage.GetNode<Player>("Player");
+		_coyoteTimer.Timeout += () =>
+		{
+			_canJump = false;
+			_isCoyoteTimerRunning = false;
+			_canCoyoteTimerStart = false;
+		};
+		_jumpBufferTimer.Timeout += () => _willJump = false;
 	}
 	protected override void PhysicsUpdate(double delta)
 	{
 		Vector2 velocity = _player.Velocity;
-		bool isOnFloor = _player.IsOnFloor();
-
-		if (!isOnFloor)
+		_isOnFloor = _player.IsOnFloor();
+		HandleCoyoteTime();
+		HandleJumpBuffer();
+		
+		if (_isOnFloor)
+			_canJump = true;
+		else
 			velocity += _player.GetGravity() * (float)delta * 0.5f;
 
-		if (Input.IsActionJustPressed("Jump") && isOnFloor)
+		if ((Input.IsActionJustPressed("Jump") && _canJump) || (_willJump && _isOnFloor))
+		{
 			velocity.Y = -JumpVelocity;
+			_canJump = false;
+			_willJump = false;
+		}
 
 		int direction = 0;
 		if (Input.IsActionPressed("Right"))
@@ -34,8 +56,29 @@ public partial class Player_MoveControlState : State
 
 		if (direction != 0)
 			Storage.SetVariant("HeadingLeft", direction < 0);
-			
+
 		_player.Velocity = velocity;
 		_player.MoveAndSlide();
+	}
+	private void HandleCoyoteTime()
+	{
+		if (_isCoyoteTimerRunning) return;
+		if (_isOnFloor)
+		{
+			_canCoyoteTimerStart = true;
+		}
+		else if (_canCoyoteTimerStart)
+		{
+			_coyoteTimer.Start();
+			_isCoyoteTimerRunning = true;
+		}
+	}
+	private void HandleJumpBuffer()
+	{
+		if (Input.IsActionJustPressed("Jump") && !_isOnFloor)
+		{
+			_willJump = true;
+			_jumpBufferTimer.Start();
+		}
 	}
 }
