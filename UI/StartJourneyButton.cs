cbb0cb9dc6333bac44b759ea4ff91f728898a3a7
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2025-10-05 17:53:03 +0800

    Make StartJourney button directs to test level, also anchored TransitionLayer's ColorRect to Full Rect

---
+++ UI/StartJourneyButton.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class StartJourneyButton : ResponsiveButton
+{
+	public override void _Ready()
+	{
+		Pressed += () => SceneManager.Instance.ChangeScene("res://Levels/TestLevel.tscn");
+	}
+}
