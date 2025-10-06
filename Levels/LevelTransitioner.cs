a102cfb574a4c2932792995a39136c24e78d06d3
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2025-10-06 16:26:36 +0800

    Added spike tile and level transitioner, worked on normal levels

---
+++ Levels/LevelTransitioner.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class LevelTransitioner : Area2D
+{
+	[Export] public string TargetLevelPath = "";
+	public override void _Ready()
+	{
+		BodyEntered += (Node2D body) =>
+		{
+			if (body is Player)
+				SceneManager.Instance.ChangeScene(TargetLevelPath);
+		};
+    }
+}
