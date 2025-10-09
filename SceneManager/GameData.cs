0a159b59d30f127f46a0397f52060e191fc38bc2
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2025-10-09 12:11:52 +0800

    Added some more player stats

--- SceneManager/GameData.cs
+++ SceneManager/GameData.cs
@@ -9,7 +9,9 @@ public partial class GameData : Node
     public int PlayerMaxHealth = 3;
     public int PlayerShield = 1;
     public int PlayerMaxJumps = 2;
+    public int PlayerMaxDashes = 2;
     public float PlayerEvasionChance = 0f;
     public float PlayerCritChance = 0f;
     public float PlayerCritDamageMultiplier = 1f;
+    public float PlayerAttackSpeedMultiplier = 1f;
 }
