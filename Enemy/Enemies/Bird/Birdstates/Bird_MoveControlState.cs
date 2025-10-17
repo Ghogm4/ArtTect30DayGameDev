5cb80bb094a1e5f18d2acd4ff891e169b9669cdf
Reyar <2290197845@qq.com>
2025-10-18 04:26:41 +0800

    Added Suspicious Bird Dive

--- Enemy/Enemies/Bird/Birdstates/Bird_MoveControlState.cs
+++ Enemy/Enemies/Bird/Birdstates/Bird_MoveControlState.cs
@@ -8,12 +8,22 @@ public partial class Bird_MoveControlState : State
     private AnimatedSprite2D _sprite = null;
     [Export] public float Speed = 100f;
     [Export] public float Acceleration = 100f;
-    [Export] public float Deceleration = 500f;
+    [Export] public float Deceleration = 10f;
     [Export] public float MaxForce = 300f;
     [Export] public float TurnSpeed = 80f;
 
+    [Export] public float AttackCD = 5f;
+    [Export] public float DiveSpeed = 400f;
+
     private float _currentRotation = 0f;
     private float _targetRotation = 0f;
+    private bool _isPreparing = false;
+    private bool _isDiving = false;
+    private float _attackTimer = 0f;
+    private float _prepareTimer = 0f;
+    private Vector2 _lastPos = Vector2.Zero;
+    private float _diveWay = 0f;
+    private float _diveLength = 0f;
 
     
 
@@ -40,7 +50,43 @@ public partial class Bird_MoveControlState : State
             pastPlayerPosition = _player.GlobalPosition;
             pastTime = 0;
         }
-        if (Storage.GetVariant<bool>("Is_Chasing"))
+        if (!_isPreparing && !_isDiving)
+        {
+            _attackTimer += (float)delta;
+            if (_attackTimer >= AttackCD)
+            {
+                _isPreparing = true;
+                _prepareTimer = 0f;
+                _attackTimer = 0f;
+            }
+        }
+        else if (_isPreparing)
+        {
+            flash();
+            _prepareTimer += (float)delta;
+            if (_prepareTimer >= 0.3f)
+            {
+                _isPreparing = false;
+                _isDiving = true;
+                _lastPos = _player.GlobalPosition;
+                _diveLength = _enemy.GlobalPosition.DistanceTo(_lastPos);
+            }
+        }
+        else if (_isDiving)
+        {
+            Vector2 direction = (_lastPos - _enemy.GlobalPosition).Normalized();
+            velocity = direction * DiveSpeed;
+            _diveWay += DiveSpeed * (float)delta;
+            _enemy.Velocity = velocity;
+            if (_diveWay - _diveLength >= 50f || _enemy.IsOnFloor())
+            {
+                _isDiving = false;
+                _diveWay = 0f;
+
+            }
+            _targetRotation = velocity.Angle();
+        }
+        if (Storage.GetVariant<bool>("Is_Chasing") && !_isPreparing && !_isDiving)
         {
             Vector2 direction = (pastPlayerPosition - _enemy.GlobalPosition).Normalized();
             Vector2 desiredVelocity = direction * Speed;
@@ -71,6 +117,20 @@ public partial class Bird_MoveControlState : State
 
     protected override void FrameUpdate(double delta)
     {
+        Rotate((float)delta);
+        
+        
+        
+    }
+
+    private void OnChase()
+    {
+        GD.Print("Chase event triggered");
+        Storage.SetVariant("Is_Chasing", true);
+    }
+
+    private void Rotate(float delta)
+    {
         if (Storage.GetVariant<bool>("HeadingLeft"))
         {
             _sprite.FlipH = true;
@@ -89,16 +149,11 @@ public partial class Bird_MoveControlState : State
         {
             _sprite.FlipV = false;
         }
-        
-        
-        
     }
-
-    private void OnChase()
+    
+    public void flash()
     {
-        GD.Print("Chase event triggered");
-        Storage.SetVariant("Is_Chasing", true);
+        
     }
-
     
 }
