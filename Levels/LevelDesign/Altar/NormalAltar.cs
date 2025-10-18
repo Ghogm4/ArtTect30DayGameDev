67cba255e19cf2794c280a51e714a5786c63c402
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2025-10-19 03:29:46 +0800

    Added normal altar

---
+++ Levels/LevelDesign/Altar/NormalAltar.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class NormalAltar : Node2D
+{
+	[Export] public Sprite2D AltarSprite;
+	[Export] public DropTable BoostDropTable;
+	private bool _isInteractable = true;
+	private bool _isPlayerNearby = false;
+	public void OnBodyEntered(Node2D body)
+	{
+		if (!body.IsInGroup("Player"))
+			return;
+		if (_isInteractable)
+			ToggleWhiteOutline(true);
+		_isPlayerNearby = true;
+	}
+	public void OnBodyExited(Node2D body)
+	{
+		if (!body.IsInGroup("Player"))
+			return;
+		ToggleWhiteOutline(false);
+		_isPlayerNearby = false;
+	}
+	public override void _Process(double delta)
+	{
+		if (_isInteractable && _isPlayerNearby && Input.IsActionJustPressed("Interact"))
+		{
+			BoostDropTable.Drop();
+			_isInteractable = false;
+			ToggleWhiteOutline(false);
+		}
+	}
+	private void ToggleWhiteOutline(bool enabled)
+	{
+		ShaderMaterial altarMaterial = AltarSprite.Material as ShaderMaterial;
+		altarMaterial.SetShaderParameter("outline_enabled", enabled);
+	}
+}
