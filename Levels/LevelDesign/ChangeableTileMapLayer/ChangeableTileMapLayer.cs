af950dee30583f6b947743c3f90b180f3a3ff1be
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2025-11-09 10:33:40 +0800

    Modified ChangeableTileMapLayer

--- Levels/LevelDesign/ChangeableTileMapLayer/ChangeableTileMapLayer.cs
+++ Levels/LevelDesign/ChangeableTileMapLayer/ChangeableTileMapLayer.cs
@@ -3,5 +3,10 @@ using System;
 
 public partial class ChangeableTileMapLayer : TileMapLayer
 {
-	public void PutCell(Vector2I cellPos, Vector2I cellAtlasPos) => SetCell(cellPos, 0, cellAtlasPos, 0);
+	public void PutCell(Vector2I cellPos, Vector2I cellAtlasPos, int width, int height)
+	{
+		for (int i = 0; i < width; i++)
+			for (int j = 0; j < height; j++)
+				SetCell(cellPos + new Vector2I(i, j), 0, cellAtlasPos, 0);
+	}
 }
